23cb851d51fd97b7a3e0ab175ce88e94d7c0786e
Mafeeer <fer.landa.unam@gmail.com>
2025-04-07 23:14:21 -0600

    Segunda foto

--- prueba.java
+++ prueba.java
@@ -3,7 +3,9 @@ public class prueba {
     public static void main(String[] args) {
         
         System.out.println("Hola mundo :)");
+        System.out.println("Adios mundo cruel");
 
 
+        
     }
 }
